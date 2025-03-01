056c3b5bd8ad814dfe5f654788dce801872db3b1
fuuzen <fuuzen@users.noreply.github.com>
2025-03-01 04:08:52 +0000

    deploy: 8614edd9032711e32d7c9683e6c9a3abb3ad971d

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fyi.taf.PersonalTax"}];updateSearchResults();
--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fyi.taf.PersonalTax","l":"Calculator"},{"p":"fyi.taf.PersonalTax","l":"programGUI"}];updateSearchResults();
